743ee00ea3134ebe8cca7bbe6a7745df3718ed65
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-07-29 00:53:36 +0300

    Question20

--- G-NET-60-LINQ02/Program.cs
+++ G-NET-60-LINQ02/Program.cs
@@ -276,6 +276,26 @@ internal class Program
 
         #endregion
 
+        #region Question20
+
+        //20. Concatenate the first 3 product names with the first 3  customer company names into a single sequence.
+
+    //    var result = ProductList
+    //.Select(p => p.ProductName)
+    //.Take(3)
+    //.Concat(CustomerList.Select(c => c.CompanyName).Take(3));
+
+    //    foreach (var item in result)
+    //    {
+    //        Console.WriteLine(item);
+    //    }
+
+
+
+
+
+        #endregion
+
 
 
 
