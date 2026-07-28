f32a3cdf731e5e8a1543e66c389bb871cdcb9b70
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-07-28 22:54:20 +0300

    Question01

--- G-NET-60-LINQ02/Program.cs
+++ G-NET-60-LINQ02/Program.cs
@@ -1,12 +1,29 @@
-﻿namespace G_NET_60_LINQ02
-{
+﻿namespace G_NET_60_LINQ02;
+using static G_NET_60_LINQ02.Source;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-         
+
+            #region Question01
+            //1. Get top 3 most expensive products
+
+        //    var top = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+
+        //foreach (var item in top)
+        //{
+        //    Console.WriteLine(item);
+        //}
 
 
-        }
+
+
+        #endregion
+
+
+
+
     }
-}
+    }
+
