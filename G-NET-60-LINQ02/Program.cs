52fbd5e19e1d2f321cbbf8678ca4c059092424ba
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-07-28 23:36:19 +0300

    Question07

--- G-NET-60-LINQ02/Program.cs
+++ G-NET-60-LINQ02/Program.cs
@@ -75,7 +75,7 @@ using static G_NET_60_LINQ02.Source;
 
 
 
-        #endregion 
+        #endregion
 
         //معلش مخدش بالي من الليستا اللي انتو كاتبنها بس هي هي نفس الفكرة  Q 05  :)
 
@@ -89,6 +89,24 @@ using static G_NET_60_LINQ02.Source;
         //{
         //    Console.WriteLine(group);
         //}
+
+        #endregion
+
+        #region Question07
+        //7. Group products by Category and project only product names per group.
+
+        var GroupProductNames = ProductList.GroupBy(p => p.Category)
+                                         .Select(g => new { g.Key, ProductNames = g.Select(p => p.ProductName) });
+
+        foreach (var group in GroupProductNames)
+        {
+            Console.WriteLine($"Category: {group.Key}");
+            foreach (var productName in group.ProductNames)
+            {
+                Console.WriteLine($" - {productName}");
+            }
+        }
+
 
         #endregion
 
