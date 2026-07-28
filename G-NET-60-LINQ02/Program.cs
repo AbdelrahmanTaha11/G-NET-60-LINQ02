218ced2db82ce4f9cbb46e15224bd868e074378d
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-07-29 00:17:59 +0300

    Question13

--- G-NET-60-LINQ02/Program.cs
+++ G-NET-60-LINQ02/Program.cs
@@ -180,6 +180,22 @@ using static G_NET_60_LINQ02.Source;
 
         #endregion
 
+        #region Question13
+        //13. find product IDs that are in setA but NOT in setB
+        //int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
+        //int[] setB = { 3, 6, 9, 12, 15, 13 };
+
+        //var productIDsInSetAOnly = setA.Except(setB);
+
+        //foreach (var productID in productIDsInSetAOnly)
+        //{
+        //    Console.WriteLine(productID);
+        //}
+
+
+
+        #endregion
+
 
 
 
